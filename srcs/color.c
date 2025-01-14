bf481c63e5eff0ae429106d8b8f22693efb75b3e
Yui Mizukami <ymizukam@c4r3s13.42tokyo.jp>
2025-01-14 21:29:08 +0900

    RGBが255以上のときは255にするように設定

--- srcs/color.c
+++ srcs/color.c
@@ -6,7 +6,7 @@
 /*   By: ymizukam <ymizukam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 11:43:19 by ymizukam          #+#    #+#             */
-/*   Updated: 2025/01/11 20:39:34 by ymizukam         ###   ########.fr       */
+/*   Updated: 2025/01/14 21:21:39 by ymizukam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ unsigned int	color_convert(t_scaled_col scol)
 	unsigned int	green;
 	unsigned int	blue;
 
-	if (scol.x > 1 || scol.y > 1 || scol.z > 1)
-		WARNIG;
-	red = scol.x * 255;
-	green = scol.y * 255;
-	blue = scol.z * 255;
+	// if (scol.x > 1 || scol.y > 1 || scol.z > 1)
+	// 	WARNIG;
+	red = fmin(255, scol.x * 255);
+	green = fmin(255, scol.y * 255);
+	blue = fmin(255, scol.z * 255);
 	return (red << 16 | green << 8 | blue);
 }
 
