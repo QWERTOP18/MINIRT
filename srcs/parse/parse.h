1d480510b865709b7fa895b6e5fed2450a36ba37
Yui Mizukami <ymizukam@c6r4s5.42tokyo.jp>
2025-04-08 01:09:18 +0900

    add getnextline

--- srcs/parse/parse.h
+++ srcs/parse/parse.h
@@ -6,16 +6,20 @@
 /*   By: ymizukam <ymizukam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 01:37:59 by ymizukam          #+#    #+#             */
-/*   Updated: 2025/01/06 03:21:00 by ymizukam         ###   ########.fr       */
+/*   Updated: 2025/04/08 01:08:52 by ymizukam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
+# include "../incs/system.h"
+# include "get_next_line.h"
 # include "libft.h"
-# include "system.h"
 
+/* ---------------------------   外部呼び出し   ------------------------------- */
 t_objects	*parse_file(char *file, t_sys *sys);
+
+/* -----------------------------  内部関数   --------------------------------- */
 
 #endif
