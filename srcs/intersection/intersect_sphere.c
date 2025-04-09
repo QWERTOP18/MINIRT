467c4e86daad7dc291298248b167ac9c004ec909
Yui Mizukami <ymizukam@c4r2s14.42tokyo.jp>
2025-04-10 08:09:55 +0900

    cylinderうまくいかない。。。

--- srcs/intersection/intersect_sphere.c
+++ srcs/intersection/intersect_sphere.c
@@ -6,7 +6,7 @@
 /*   By: ymizukam <ymizukam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 13:53:30 by ymizukam          #+#    #+#             */
-/*   Updated: 2025/04/10 02:14:33 by ymizukam         ###   ########.fr       */
+/*   Updated: 2025/04/10 07:44:11 by ymizukam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ t_intersect	is0(t_unit_line ray, void *obj)
 	t_vec			camera_center;
 	double			b;
 	double			c;
+	t_vec			roots;
 	const t_sphere	*sp = (const t_sphere *)obj;
 
 	camera_center = vec_sub(ray.pos, sp->center);
 	b = 2 * vec_dot(ray.dir, camera_center);
 	c = vec_dot(camera_center, camera_center) - sp->radius;
-	if (b * b - 4 * 1 * c < 0)
+	roots = solve_eq(1, b, c);
+	if (roots.x < 1)
 		is.dist = __DBL_MAX__;
 	else
 	{
-		is.dist = (-b - sqrt(b * b - 4 * 1 * c)) / 2;
+		is.dist = roots.y;
 		is.pos = vec_add(ray.pos, vec_mul(ray.dir, is.dist));
 		is.normal = vec_normalize(vec_sub(is.pos, sp->center));
 	}
